e790bb59fb29eeb73d2f64ea07932125e973c276
Tim van Lieshout <tbliesho@avans.nl>
2017-10-26 13:15:52 +0200

    first steps database

---
+++ app/src/main/java/com/example/tim/contactcardapp/data/local/UsersDBHelper.java
@@ -0,0 +1,8 @@
+package com.example.tim.contactcardapp.data.local;
+
+/**
+ * Created by tim on 26-10-2017.
+ */
+
+public class UsersDBHelper {
+}
---
+++ app/src/main/java/com/example/tim/contactcardapp/data/model/User.java
@@ -0,0 +1,8 @@
+package com.example.tim.contactcardapp.data.model;
+
+/**
+ * Created by tim on 26-10-2017.
+ */
+
+public class User {
+}
